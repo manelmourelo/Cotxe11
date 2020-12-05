2455c22cf42e85498a78cf199299b59187965bd6
David Valdivia <d.valdiviamartinez@gmail.com>
2020-12-05 20:21:39 +0100

    Bounce logic done

--- Cotxe11/Assets/Scripts/Character/CharacterController.cs
+++ Cotxe11/Assets/Scripts/Character/CharacterController.cs
@@ -7,6 +7,7 @@ public class CharacterController : MonoBehaviour
     public float speed = 1.0f;
     public float jump_force = 200.0f;
     public float max_speed = 5.0f;
+    public float bounce_multiplier = 3.0f;
 
     private bool facing_right = true;
     private bool on_air = false;
@@ -76,6 +77,14 @@ public class CharacterController : MonoBehaviour
         {
             current_jumps = 0;
             on_air = false;
+        }
+
+
+        if (collision.gameObject.tag == "Bounce")
+        {
+            on_air = true;
+            character_rb.velocity = new Vector2(0.0f, 0.0f);
+            character_rb.AddForce(Vector2.up * jump_force * bounce_multiplier, ForceMode2D.Impulse);
         }
     }
 
