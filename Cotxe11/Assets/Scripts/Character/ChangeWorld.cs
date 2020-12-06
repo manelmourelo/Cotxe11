5cc69345133495bc86749c75a86b702f4568613c
David Valdivia <d.valdiviamartinez@gmail.com>
2020-12-06 11:49:37 +0100

    Change World script

---
+++ Cotxe11/Assets/Scripts/Character/ChangeWorld.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ChangeWorld : MonoBehaviour
+{
+    public GameObject PlayerDay = null;
+    public GameObject PlayerNight = null;
+    public GameObject CMCamera = null;
+
+    private CinemachineVirtualCamera CMVirtualCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CMVirtualCam = CMCamera.GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerDay")
+        {
+            PlayerDay.SetActive(false);
+            PlayerNight.SetActive(true);
+
+            CMVirtualCam.Follow = PlayerNight.transform;
+        }
+    }
+
+
+}
