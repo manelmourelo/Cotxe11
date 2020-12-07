03226e44aa9831513d7f24c3ecb4fbc1bad6d9ec
manelmourelo <manelmourelo.1@gmail.com>
2020-12-07 15:59:56 +0100

    PauseMenu

---
+++ Cotxe11/Assets/Scripts/UI/PauseMenuUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        transform.parent.gameObject.SetActive(false);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
